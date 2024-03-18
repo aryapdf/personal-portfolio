c62f6d1aecb748dc6f3d48867cad39b88ea5900f
Arya Agni Pradana Firdaus <aryaagnipradana@gmail.com>
2024-03-18 07:06:03 +0700

    fix code at CV Section

--- src/components/CV.js
+++ src/components/CV.js
@@ -7,46 +7,61 @@ import React from "react";
 
 import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import { FaCommentDots } from "react-icons/fa";
+import { FaBriefcase, FaCommentDots, FaGraduationCap } from "react-icons/fa";
 
 const CV = () => {
   //Make Download CV Function
 
   return (
     <section className="CV">
+      <h2>Experience</h2>
       <div className="container">
         <div className="content">
-          <h2>Experience</h2>
+          <p>
+            <FaBriefcase />
+          </p>
           <div className="exp">
-            <h3>Front-End Developer at Dicoding</h3>
-            <p>Internship, Aug 2022 - Dec 2022</p>
+            <p>Aug 2022 - Dec 2022</p>
+            <h3>Front-End Developer, Dicoding</h3>
+            <p>Internship</p>
           </div>
-          <div className="border"></div>
           <div className="exp">
+            <p>Apr 2022 - Aug 2022</p>
             <h3>Secretarial Staff, Faculty of Agriculture</h3>
             <p>Part Time, Apr 2022 - Aug 2022</p>
           </div>
+          <div className="exp">
+            <p>May 2022 - July 2022</p>
+            <h3>IT Support, Digitalent FGA Program</h3>
+            <p>Training Participant</p>
+          </div>
         </div>
         <div className="content">
-          <h2>Education</h2>
+          <p>
+            <FaGraduationCap />
+          </p>
           <div className="exp">
+            <p>2019 - 2023</p>
             <h3>University of Jambi</h3>
-            <p>Bachelor of Agribusiness, 2019 - 2023</p>
+            <p>Bachelor of Agribusiness </p>
           </div>
-          <div className="border"></div>
           <div className="exp">
+            <p>2016 - 2019</p>
             <h3>SMA N 3 Cikarang Utara</h3>
-            <p>Senior High School, 2016 - 2019</p>
+            <p>Senior High School</p>
           </div>
         </div>
         <div className="button">
-          <Link className="btn btn-secondary" to='./about'>
+          <Link className="btn btn-secondary" to="./about">
             About Me <FaArrowUpRightFromSquare size={16} />
           </Link>
         </div>
         <div className="selected">
           <h2>Selected Works</h2>
-          <p>These are three of my favorite projects. Or at least.. recent projects i have been worked on.</p>
+          <p>
+            These are three of my favorite projects. Or at least.. recent
+            projects i have been worked on.
+          </p>
           <div className="project-container">
             {WorkCardData.slice(0, 3).map((val, ind) => {
               return (
